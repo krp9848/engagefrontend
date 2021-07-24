017c2e4c9446ee4c50044539696affba195a6b27
krp9848 <kabirajpant@gmail.com>
2021-07-24 23:50:36 +0530

    feat: search user selectable

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { PROFILE } from '../../navigation/CONSTANT'
 import './User.scss'
 
 const User = ({ user: { name, username } }) => {
   return (
     <div className="user">
-      <div className="user-image">
-        <img
-          src="https://krp9848.github.io/portfolio/static/media/profile.a110496e.jpeg"
-          alt="profile image"
-        />
-      </div>
-      <div className="user-details">
-        <p className="name">{name}</p>
-        <p className="username">&#64;{username}</p>
-      </div>
+      <Link to={`${PROFILE}/${username}`}>
+        <div className="user-image">
+          <img
+            src="https://krp9848.github.io/portfolio/static/media/profile.a110496e.jpeg"
+            alt="profile image"
+          />
+        </div>
+        <div className="user-details">
+          <p className="name">{name}</p>
+          <p className="username">&#64;{username}</p>
+        </div>
+      </Link>
     </div>
   )
 }
